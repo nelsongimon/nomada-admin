8865771f20a470f53d0fa0742c3f08a458886400
Nelson Gimon <nelson.gimon@gmail.com>
2023-10-23 13:06:54 -0400

    Delete all images

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
 
 Route::get('/search/{search}', [ApiController::class, 'search']);
 Route::get('/featured-products', [ApiController::class, 'featuredProducts']);
